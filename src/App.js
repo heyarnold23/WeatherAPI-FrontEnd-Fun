e1409cfd74524eac99591c43d4e259f87d1726f4
Arnold C <acabang23@gmail.com>
2021-12-15 16:13:49 -0800

    all work non committed that shows current day weather

--- src/App.js
+++ src/App.js
@@ -1,8 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+// api key 11d3da09cb0c13434008de6f917c6f97
+
 
 function App() {
+  const [weather, setWeather] = useState();
+  const [icon, setIcon] = useState();
+  const [highTemp, setHighTemp] = useState();
+  const [lowTemp, setLowTemp] = useState();
+
+
+
+
+  const getApi = async () => {
+    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?zip=95210,us&appid=11d3da09cb0c13434008de6f917c6f97');
+    const data = await response.json();
+    console.log(data);
+    setWeather(data.weather[0])
+    setHighTemp(Math.round((data?.main?.temp_max - 273.15) * 9/5 + 32) + '°')
+    setLowTemp(Math.round((data?.main?.temp_min - 273.15) * 9/5 + 32) + '°')
+  }
+
+  useEffect(() => {
+    getApi();
+    if (weather?.icon === '10n') {
+      setIcon('https://openweathermap.org/img/wn/10n@2x.png')
+    }
+
+  }, [weather?.icon]);
+
+  console.log(weather);
+  console.log(icon)
+  console.log(highTemp)
+
+
+  const days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  const day = new Date();
+  const currentDay = days[day.getDay()];
+  console.log(currentDay)
+
   return (
-    <h1>Hello world!</h1>
+    <>
+      <div id='card'>
+        <span id='day'>
+          {currentDay}
+        </span>
+        <div id='icon'>
+          <img src={`${icon}`} alt="" />
+        </div>
+        <div id='temps'>
+          <div id='highTemp'>
+            {highTemp}
+          </div>
+          <div id='lowTemp'>
+            {lowTemp}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
