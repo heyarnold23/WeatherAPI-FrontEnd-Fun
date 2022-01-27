fb2cd66b70e3f5f8290884d05d528a46b5d27853
Arnold C <acabang23@gmail.com>
2022-01-26 17:25:27 -0800

    reconfigured app to utilize react router v6

--- src/App.js
+++ src/App.js
@@ -1,60 +1,13 @@
-import React, { useEffect, useState } from 'react';
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import WeatherPage from "./components/WeatherPage";
 
 function App() {
-  const [weather, setWeather] = useState();
-  const [icon, setIcon] = useState();
-  const [highTemp, setHighTemp] = useState();
-  const [lowTemp, setLowTemp] = useState();
-
-
-
-
-  const getApi = async () => {
-    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?zip=95210,us&appid=11d3da09cb0c13434008de6f917c6f97');
-    const data = await response.json();
-    console.log(data);
-    setWeather(data.weather[0])
-    setHighTemp(Math.round((data?.main?.temp_max - 273.15) * 9/5 + 32) + '°')
-    setLowTemp(Math.round((data?.main?.temp_min - 273.15) * 9/5 + 32) + '°')
-  }
-
-  useEffect(() => {
-    getApi();
-    setIcon(`https://openweathermap.org/img/wn/${weather?.icon}@2x.png`)
-
-  }, [weather?.icon]);
-
-  console.log(weather);
-  console.log(icon)
-  console.log(highTemp)
-
-
-  const days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
-  const day = new Date();
-  const currentDay = days[day.getDay()];
-  console.log(currentDay)
-
   return (
-    <>
-      <div id='card'>
-        <span id='day'>
-          {currentDay}
-        </span>
-        <div id='icon'>
-          <img src={`${icon}`} alt="" />
-        </div>
-        <div id='temps'>
-          <div id='highTemp'>
-            {highTemp}
-          </div>
-          <div id='lowTemp'>
-            {lowTemp}
-          </div>
-        </div>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<WeatherPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/WeatherPage/index.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+
+
+
+function App() {
+  const [weather, setWeather] = useState();
+  const [icon, setIcon] = useState();
+  const [highTemp, setHighTemp] = useState();
+  const [lowTemp, setLowTemp] = useState();
+
+
+
+
+  const getApi = async () => {
+    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?zip=95210,us&appid=11d3da09cb0c13434008de6f917c6f97');
+    const data = await response.json();
+    console.log(data);
+    setWeather(data.weather[0])
+    setHighTemp(Math.round((data?.main?.temp_max - 273.15) * 9/5 + 32) + '°')
+    setLowTemp(Math.round((data?.main?.temp_min - 273.15) * 9/5 + 32) + '°')
+  }
+
+  useEffect(() => {
+    getApi();
+    setIcon(`https://openweathermap.org/img/wn/${weather?.icon}@2x.png`)
+
+  }, [weather?.icon]);
+
+  console.log(weather);
+  console.log(icon)
+  console.log(highTemp)
+
+
+  const days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  const day = new Date();
+  const currentDay = days[day.getDay()];
+  console.log(currentDay)
+
+  return (
+    <>
+      <div id='card'>
+        <span id='day'>
+          {currentDay}
+        </span>
+        <div id='icon'>
+          <img src={`${icon}`} alt="" />
+        </div>
+        <div id='temps'>
+          <div id='highTemp'>
+            {highTemp}
+          </div>
+          <div id='lowTemp'>
+            {lowTemp}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
