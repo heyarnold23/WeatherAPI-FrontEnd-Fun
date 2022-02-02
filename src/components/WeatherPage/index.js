8d6586b00764900f9d732a585a21e62d19a2aa94
Arnold C <acabang23@gmail.com>
2022-02-01 17:01:56 -0800

    got rid of some usestates to just do calculations in jsx instead

--- src/components/WeatherPage/index.js
+++ src/components/WeatherPage/index.js
@@ -5,18 +5,14 @@ import './WeatherPage.css'
 function WeatherPage() {
   const [weather, setWeather] = useState();
   const [icon, setIcon] = useState();
-  const [highTemp, setHighTemp] = useState();
-  const [lowTemp, setLowTemp] = useState();
 
   const getApi = async () => {
     // const response = await fetch('http://api.openweathermap.org/data/2.5/weather?zip=95210,us&appid=11d3da09cb0c13434008de6f917c6f97');
     const response = await fetch('https://api.openweathermap.org/data/2.5/onecall?lat=36.812995&lon=-119.742846&appid=11d3da09cb0c13434008de6f917c6f97');
     const data = await response.json();
-    console.log('RIGHT HERE',data?.current);
+    console.log('RIGHT HERE',data);
     setWeather(data)
     setIcon(`https://openweathermap.org/img/wn/${data?.current?.weather[0].icon}@2x.png`)
-    setHighTemp(Math.round((data?.current?.feels_like - 273.15) * 9/5 + 32) + '°')
-    setLowTemp(Math.round((data?.current?.temp - 273.15) * 9/5 + 32) + '°')
   }
 
   useEffect(() => {
@@ -27,7 +23,7 @@ function WeatherPage() {
   const days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
   const day = new Date();
   const currentDay = days[day.getDay()];
-  console.log(currentDay)
+  // console.log(currentDay)
 
   return (
     <>
@@ -38,14 +34,15 @@ function WeatherPage() {
         <div className='icon'>
           <img src={`${icon}`} alt="" />
         </div>
-        <div className='temps'>
-          <div className='highTemp'>
+        <div className='tempsContainer'>
+          <div className='tempDiv'>
             <span>Feels Like</span>
-            {highTemp}
+            {`${Math.round((weather?.current?.feels_like - 273.15) * 9/5 + 32)}°`}
           </div>
-          {/* <div className='lowTemp'>
-            {lowTemp}
-          </div> */}
+          <div className='tempDiv actual'>
+            <span>Actual</span>
+          {`${Math.round((weather?.current?.temp - 273.15) * 9/5 + 32)}°`}
+          </div>
         </div>
       </div>
     </>
