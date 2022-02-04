13a7f30eec05481a9458580f94f1e62b86bb0e61
Arnold C <acabang23@gmail.com>
2022-02-03 21:50:11 -0800

    changed from inputting zip to city and state. also modal automatically pops up if no lati or long in visitors local storage yet

--- src/components/WeatherPage/index.js
+++ src/components/WeatherPage/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Modal from 'react-modal';
+import Modal from "react-modal";
 
 import "./WeatherPage.css";
 
@@ -7,11 +7,13 @@ function WeatherPage() {
   const [weather, setWeather] = useState();
   const [icon, setIcon] = useState();
   const [modalIsOpen, setIsOpen] = useState(false);
+  const [city, setCity] = useState();
+  const lati = localStorage?.getItem("lati");
+  const long = localStorage?.getItem("long");
 
   const getApi = async () => {
-    // const response = await fetch('http://api.openweathermap.org/data/2.5/weather?zip=95210,us&appid=11d3da09cb0c13434008de6f917c6f97');
     const response = await fetch(
-      "https://api.openweathermap.org/data/2.5/onecall?lat=36.812995&lon=-119.742846&appid=11d3da09cb0c13434008de6f917c6f97"
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lati}&lon=${long}&appid=11d3da09cb0c13434008de6f917c6f97`
     );
     const data = await response.json();
     console.log("RIGHT HERE", data);
@@ -23,7 +25,10 @@ function WeatherPage() {
 
   useEffect(() => {
     getApi();
-  }, []);
+    if (!lati || !long) {
+      setIsOpen(true);
+    }
+  }, [lati, long]);
 
   const days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
 
@@ -42,7 +47,22 @@ function WeatherPage() {
     setIsOpen(false);
   }
 
-  Modal.setAppElement('#root')
+  const submitCity = async () => {
+    let cool = city?.replace(" ", "%20");
+    let modded = cool.replace(",", "%2C");
+
+    const response = await fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${modded}.json?types=place%2Cpostcode%2Caddress&access_token=pk.eyJ1IjoiaGV5YXJub2xkMjMiLCJhIjoiY2t6N3hodzluMWV6bTJvcGhsaDRoZHZoYiJ9.c5nE5KuFZ7xfQZHz-tL_uA`
+      // https://api.mapbox.com/geocoding/v5/mapbox.places/San%20Francisco.json?types=place%2Cpostcode%2Caddress&access_token=pk.eyJ1IjoiaGV5YXJub2xkMjMiLCJhIjoiY2t6N3hodzluMWV6bTJvcGhsaDRoZHZoYiJ9.c5nE5KuFZ7xfQZHz-tL_uA
+    );
+    const data = await response.json();
+    const coords = data?.features[0]?.geometry?.coordinates;
+    localStorage.setItem("lati", coords[1]);
+    localStorage.setItem("long", coords[0]);
+    closeModal();
+  };
+
+  Modal.setAppElement("#root");
 
   return (
     <>
@@ -53,7 +73,13 @@ function WeatherPage() {
         // contentLabel="Example Modal"
         className="modal"
       >
-        <div>Enter ZIP</div>
+        <div>Enter City and State</div>
+        <input
+          type="text"
+          className="cityInput"
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button onClick={submitCity}>Submit</button>
         <button onClick={closeModal}>close</button>
       </Modal>
       <div className="card">
